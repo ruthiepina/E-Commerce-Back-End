2321f25977b5a1a1221cb7d3729eacf2a468647f
RMP <ruthiepina@gmail.com>
2024-02-04 18:14:56 -0700

    Finished starter code for models for ProductTag.js and Tag.js. Made association between Product and Category in index.js.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
-// import models
+//* import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
+//* Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
